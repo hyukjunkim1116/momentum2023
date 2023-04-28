fa275e9701a4dc3ca67c04f8127ea0b7e63a296e
Hyuk Jun Kim <nmkk1234@naver.com>
2023-04-28 17:16:34 +0900

    change background img

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["download.jpeg", "download1.jpeg", "download2.jpeg"];
+const images = ["1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
